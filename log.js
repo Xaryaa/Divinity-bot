23dd88cd43a930e4b97a0fee05c1430c33ff7c3e
Xaryaa <43631762+Xaryaa@users.noreply.github.com>
2018-09-28 02:00:16 +0200

    Create log.js

---
+++ log.js
@@ -0,0 +1,10 @@
+module.exports = class log {
+  static match (message){
+    return message.content.startsWith('!log')
+  }
+  
+  static action (message){
+    let args = message.content.split(' ')
+    args.shift()
+    message.reply('https://www.warcraftlogs.com/character/eu/archimonde/ + args.join('%20'))
+}
